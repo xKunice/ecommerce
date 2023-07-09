c51c207808f0c227d6dec9171cd1db82bc3e8a85
xKunice <132533167+xKunice@users.noreply.github.com>
2023-07-08 18:22:24 -0600

    se añade la imagen de baby-yoda se crea uyna funcion apra obtener los datos de la abse de datos json para

---
+++ assets/js/producto.js
@@ -0,0 +1,31 @@
+fetch('db.json')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+        
+    if (Array.isArray(data.productos)) {
+      data.productos.forEach((producto) => {
+        const productoDiv = document.createElement('div');
+        const contenedorProductos = document.getElementById(producto.categoria);
+        console.log(contenedorProductos);
+        productoDiv.classList.add('card');
+        productoDiv.innerHTML = `
+          <div class="contenedor__img">
+            <img src="${producto.imagen}" alt="baby yoda producto">
+          </div>
+          <div class="contenedor__Descripcion">
+            <h3>${producto.nombre}</h3>
+            <span>$${producto.precio}</span>
+            <button>Agregar al carrito</button>
+          </div>
+        `;
+        contenedorProductos.appendChild(productoDiv);
+      });
+      
+    } else {
+      console.log('El archivo JSON no contiene un array de productos');
+    }
+  })
+  .catch(error => {
+    console.log('Error al leer el archivo JSON', error);
+  });
