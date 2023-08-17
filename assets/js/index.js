a77604379b5192ba664989859531bc05a344af57
xKunice <132533167+xKunice@users.noreply.github.com>
2023-08-17 03:58:28 -0600

    se añaden las imagenes  a img bb esto con el find el que el repositorio sea mas ligeero, se realzian cambios en la base de datos linkeando las imagenes y se modifica el index html , se añade el css y los estilos, se corrige el nombre de la clase en index.js del contenedor

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,21 @@
+const slider = document.querySelector('.slider');
+const punto = document.querySelectorAll('.punto');
+
+punto.forEach((cadaPunto , i) =>{
+  punto[i].addEventListener('click',()=>{
+
+    let posicion = i;
+
+    let operacion = posicion * -50;
+    slider.style.transform = `translateX${ operacion}%`;
+    punto.forEach((cadaPunto, i)=>{
+      punto[i].classList.remove('activo');
+    })
+    punto[i].classList.add('activo');
+  })
+})
+
+
 fetch('db.json')
   .then(response => response.json())
   .then(data => {
@@ -19,10 +37,10 @@ fetch('db.json')
           <div class="contenedor__img">
             <img src="${producto.imagen}" alt="${producto.nombre}">
           </div>
-          <div class="contenedor__Descripcion">
+          <div class="contenedor__descripcion">
             <h3>${producto.nombre}</h3>
             <span>$${producto.precio}</span>
-            <button>Agregar al carrito</button>
+            <button>Ver Producto</button>
           </div>
         `;
         contenedorProductos.appendChild(productoDiv);
@@ -41,7 +59,60 @@ fetch('db.json')
     } else {
       console.log('El archivo JSON no contiene un array de productos');
     }
+    
 
+    const busqueda = document.getElementById('busqueda');
+    const btnBusqueda = document.getElementById('btnBusqueda');
+    const resultadoBusqueda = document.getElementById('resultadoBusqueda');
+    
+    btnBusqueda.addEventListener('click', (event) => {
+      const productoTienda = document.getElementById('productosTienda');
+      const productoEncontrado = data.productos.find(producto => producto.nombre === busqueda.value);
+
+      if(busqueda.value === '') {
+        alert('Ingrese un producto a buscar');
+        return;
+      }else{
+        alert('Buscando producto');
+      }
+      if(productoEncontrado){
+        productoTienda.style.display = 'none';
+        resultadoBusqueda.style.display = 'block';
+        alert('Producto encontrado');
+        resultadoBusqueda.innerHTML = `
+        <button id ="regresar">Regresar a la tienda</button>
+        <div class="contenedor__img">
+        <img src="${productoEncontrado.imagen}" alt="${productoEncontrado.nombre}">
+        </div>
+        <div class="contenedor__Descripcion">
+        <h3>${productoEncontrado.nombre}</h3>
+        <span>$${productoEncontrado.precio}</span>
+        <button>Agregar al carrito</button>
+      </div>
+        `;
+        const btnRegresar = document.getElementById('regresar');
+      if (btnRegresar) {
+        btnRegresar.addEventListener('click', (event) => {
+          productoTienda.style.display = 'block';
+          resultadoBusqueda.style.display = 'none';
+          resultadoBusqueda.innerHTML = '';
+      });
+}
+
+      }else{
+        alert('No se encontro el producto');
+    
+      }
+    
+    
+    });
+    
+
+
+
+
+
+    
   })
   .catch(error => {
     console.log('Error al leer el archivo JSON', error);
@@ -53,4 +124,5 @@ btnLogin.addEventListener('click', (event) => {
     event.preventDefault();
     window.location.href = './login.html';
 });
+
 
